f7b62efce4e0781dba79660c42b0438f67bbe2cd
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-21 23:41:39 +0300

    added the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
