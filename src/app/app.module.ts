536b81b013d45640c73d27b3684d7c9d20c4483e
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-22 00:25:07 +0300

    added bootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoalFormComponent } from './goal-form/goal-form.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalFormComponent
+   
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
