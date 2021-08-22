81df3b0e8261e0812f651c22717e17b4d1876218
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-22 23:30:54 +0300

    added toogle buttons

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,7 @@
 export class Quote {
-    id: number;
-    name: string;
-    description: string;
-
-    constructor(id: number,  name: string ,     description: string){
-        this.id=id;
-        this.name=name;
-        this.description=description;
+    showDescription: boolean;
+  constructor(public id: number,public name: string,public description: string){
+    this.showDescription=false;
     }
   }
 
