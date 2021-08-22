46e0e06c59442243f0f5d80d4a713fc5b40f0671
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-22 20:01:38 +0300

    added an eventemitter

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {quote} from '../quote'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.scss']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() 
 
   constructor() { }
 
