7f3762f43a3c8d55044b18d092f727538704cd8c
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-22 23:35:39 +0300

    added eventemitter

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import {Quote} from '../quote';
 
 @Component({
@@ -8,6 +8,11 @@ import {Quote} from '../quote';
 })
 export class QuoteDetailComponent implements OnInit {
   @Input() quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
 
   constructor() { }
