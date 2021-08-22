8ddc6193d7155754c34e183bca7dab058f329933
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-08-22 15:31:09 +0300

    added flex boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  quotes:string[];
+  // quotes:string[];
 
-  constructor(){
-    this.quotes= ['He who eats fare shall not see heaven', 'A brother will not become a comedian when his brother girl is around', 'femisim is a way of life']
-  }
+  // constructor(){
+  //   this.quotes= ['He who eats fare shall not see heaven', 'A brother will not become a comedian when his brother girl is around', 'femisim is a way of life']
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,6 +25,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
+    MatCardModule,
+    MatToolbarModule
  
     
   ],
